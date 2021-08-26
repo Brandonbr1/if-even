6368b23f6997e1f083688d45782ff1d4d391587a
Brandonbr1 <mohammedbrandon204@gmail.com>
2021-08-26 12:42:27 -0700

    first

---
+++ README.md
@@ -0,0 +1,14 @@
+### if-even
+
+## what is it
+it is a  more efficent version of [is-Even](https://github.com/samuelmarina/is-even/)
+
+## install
+## How to use it
+```js
+
+isEven(1); //false
+isEven(2); //true
+isEven(3); //false
+// And so on
+```
---
+++ index.js
@@ -0,0 +1,8 @@
+let counter = 0;
+while (counter !== Infinity) {
+    if (number === counter && counter % 2 === 1) return false;
+    else if (number === counter && counter % 2 === 1 ) return false;
+    counter++
+}  
+
+module.exports.ifEven = ifEven
