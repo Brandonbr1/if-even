f2e593c7a34b8598a04f97a7e7311736d15f0349
Brandonbr1 <mohammedbrandon204@gmail.com>
2021-08-26 14:49:53 -0700

    changed false to true

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let counter = 0;
 while (counter !== Infinity) {
-    if (number === counter && counter % 2 === 1) return false;
+    if (number === counter && counter % 2 === 1) return true;
     else if (number === counter && counter % 2 === 1 ) return false;
     counter++
 }  
